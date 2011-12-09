0b73fecff401c3cf8ca397d74181da170be65020
Kostiantyn Danylov <kdanilov@mirantis.com>
2011-12-09 19:45:50 +0200

    move compiler to new lexer a bit of updates

--- notsorest2html.py
+++ notsorest2html.py
@@ -5,132 +5,16 @@ import re
 import sys
 
 from pygments import highlight
-from pygments.lexers import PythonLexer, CLexer
+from pygments.lexers import PythonLexer, \
+                            CLexer, \
+                            XmlLexer, \
+                            PythonTracebackLexer, \
+                            PythonConsoleLexer
+
 from pygments.formatters import HtmlFormatter
 
-def indent_level(line):
-    line = line.replace('\t', ' ' * 4)
-    return len(line) - len(line.lstrip())
-    
-def is_block(block):
-    blines = block.split('\n')
-    
-    if len(blines) == 0:
-        return False
-        
-    first_ind = indent_level(blines[0])
-    
-    if first_ind == 0:
-        return True
-        
-    if len(blines) > 1 and indent_level(blines[1]) == 0:
-        return True
-    
-    return False
+from nsr_lexer import parse
 
-header_re_sub = re.compile("(?:===+)|(?:---+)$")
-def closed_block(block):
-    blines = block.split('\n')
-    if len(blines) == 2 and header_re_sub.match(blines[1]):
-        return True
-    return False
-
-a_lot_of_line_breaks = re.compile(r"\n\n+")
-def find_blocks(text):
-    curr_block = []
-    ntext = "\n".join([line.rstrip() for line in text.split('\n')])
-    
-    blocks = a_lot_of_line_breaks.split(ntext)
-    rblocks = [""]
-    
-    for block in blocks:
-        if not is_block(block) and not closed_block(rblocks[-1]):
-            rblocks[-1] += '\n\n' + block
-        else:
-            rblocks.append(block)
-    
-    return rblocks
-
-HEADER1 = 'header1'
-HEADER2 = 'header2'
-TEXT = 'text'
-LIST = 'list'
-CUT = 'cut'
-
-typed_block = re.compile("([-a-zA-Z_]+):$")
-typed_str = re.compile("[-a-zA-Z_]+:.*$")
-cut_re = re.compile(r"<---+>$")
-
-
-def classify_block(block):
-    block = block.lstrip()
-    if u'\n' not in block:
-        if typed_str.match(block):
-            yield block.split(':', 1)
-        elif block.startswith('* '):
-            yield LIST, [block[2:   ]]
-        elif cut_re.match(block):
-            yield CUT, None
-        else:
-            yield TEXT, block
-    else: 
-        fline, rest = block.split('\n', 1)
-        mres = typed_block.match(fline)
-        
-        if mres is not None:
-            yield mres.group(1), rest
-
-        elif header_re_sub.match(rest):
-            if rest[0] == '=':
-                yield HEADER1, fline
-            else:
-                yield HEADER2, fline
-        else:
-            cblock = []
-            in_list = False
-            curr_indent_level = indent_level(block)
-            list_items = []
-
-            for line in block.split('\n'):
-                if line.strip() == "":
-                    continue
-                if not in_list:
-                    if line.strip().startswith('* '):
-                        # we enter the first list item
-                        if cblock != []:
-                            yield TEXT, "\n".join(cblock)
-                            cblock = []
-                        in_list = True
-                        curr_indent_level = indent_level(line)
-                        cblock.append(line.lstrip()[2:])
-                    else:
-                        cblock.append(line)
-                else:
-                    if indent_level(line) > curr_indent_level:
-                        cblock.append(line.lstrip())
-                    elif indent_level(line) <= curr_indent_level:
-                        # new item
-                        if line.strip().startswith('* '):
-                            list_items.append("\n".join(cblock))
-                            cblock = [line.lstrip()[2:]]
-                            curr_indent_level = indent_level(line) 
-                        else:
-                            # list finished
-                            list_items.append("\n".join(cblock))
-                            yield LIST, list_items
-                            cblock = []
-                            list_items = []
-                            in_list = False 
-            if cblock != [] or in_list:
-                if in_list:
-                    if cblock != []:
-                        list_items.append("\n".join(cblock))
-                    
-                    if list_items != []:
-                        yield LIST, list_items
-                else:
-                    if cblock != []:
-                        yield TEXT, "\n".join(cblock)
 
 def deindent_snippet(snippet):
     snippet = snippet.replace('\t', ' ' * 4)
@@ -147,31 +31,23 @@ def escape_html(text, esc_all=False):
     }
 
     if esc_all:
-        html_escape_table[""] = '&#39;'
+        html_escape_table["'"] = '&#39;'
     
     return "".join( html_escape_table.get(c, c) for c in text)
 
-def highlight_python(code):
-    return highlight(code, 
-                    PythonLexer(), 
-                    HtmlFormatter(noclasses=True))
 
+re_bold    = re.compile(r"(?i)'\b(.+?)\b'")
+re_it      = re.compile(r"(?i)\*\b(.+?)\b\*")
+re_striked = re.compile(r"(?i)-\b(.+?)\b-")
 
-def highlight_c(code):
-    return highlight(code, 
-                    CLexer(), 
-                    HtmlFormatter(noclasses=True))
-
-#re_italic = re.compile(r"(?ims)'(.*?)'")
-re_bold = re.compile(r"(?ims)'(.*?)'")
-re_autor = re.compile("(?ims)^:Author:.*?$")
-re_href = re.compile(r"(?P<name>\[.*?\])?(?P<proto>https?://)(?P<url>.*?)(?=\s|$)")
+re_backref = re.compile(r"\[\s*([- _a-zA-Z]+)\s*\]")
+re_href = re.compile(r"(?P<name>\[\s*([- _a-zA-Z/.]+)\s*\])?(?P<proto>https?://)(?P<url>.*?)(?=\s|$)")
 
 class NotSoRESTHandler(object):
     def __init__(self):
         self.stream = []
     
-    def write(self, text):
+    def write_raw(self, text):
         self.stream.append(text)
     
     def get_result(self):
@@ -182,7 +58,7 @@ class NotSoRESTHandler(object):
         if style is not None:
             self.start_style(style)
 
-        getattr(self, 'on_' + tp, lambda x : None)(data)
+        getattr(self, 'on_' + tp)(data) #, lambda x : None
 
         if style is not None:
             self.end_style(style)
@@ -193,122 +69,184 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
         self.refs = []
         super(BlogspotHTMLProvider, self).__init__()
 
+    def write_text(self, text):
+        self.write_raw(self.text_to_html(text).replace('\n', ' '))
+
+    def text_to_html(self, text):
+        
+        ntext = escape_html(text)
+        
+        ntext = re_bold.sub(r"<b>\1</b>", ntext)
+        ntext = re_it.sub(r"<i>\1</i>", ntext)
+        ntext = re_striked.sub(r"<s>\1</s>", ntext)
+        ntext = re_backref.sub(self.process_backref, ntext)
+        ntext = re_href.sub(self.process_href, ntext)
+
+        return ntext
+    
     def start_style(self, style):
-        self.write('<span style="{0}">'.format(style))
+        self.write_raw('<span style="{0}">'.format(style))
     
     def end_style(self, style):
-        self.write('</span>')
+        self.write_raw('</span>')
 
     def on_text(self, block, no_para=False):
         if block != "":
             if not no_para:
-                self.write('<p style="text-indent:20px">')
+                self.write_raw('<p style="text-indent:20px">')
             
-            self.write(self.escape_html(block).replace('\n', ' '))
+            self.write_text(block)
             
             if not no_para:
-                self.write("</p>")
+                self.write_raw("</p>")
     
     def on_cut(self, block):
-        self.write("<!--more-->")
+        self.write_raw("<!--more-->")
 
     def on_raw(self, block):
-        self.write('<pre><font face="courier">' + 
-                    escape_html(block, esc_all=True) + 
+        self.write_raw('<pre><font face="courier">' + 
+                       escape_html(block, esc_all=True) + 
                         '</font></pre>')
 
-    def on_traceback(self, block):
-        self.write('<pre><font face="courier">' + 
-                    escape_html(block, esc_all=True) + 
-                        '</font></pre>')
+    highlighters_map = {}
+    highlighters_map['python'] = PythonLexer
+    highlighters_map['c'] = CLexer
+    highlighters_map['xml'] = XmlLexer
+    highlighters_map['traceback'] = PythonTracebackLexer
+    highlighters_map['pyconsole'] = PythonConsoleLexer
 
-    def on_python(self, block):
-        self.write(highlight_python(deindent_snippet(block)).strip())
-    
+    def __getattr__(self, name):
+        # handle all syntax hightlited blocks
+        if name.startswith('on_'):
+            block = name[3:]
+            if block in self.highlighters_map:
+                lexer = self.highlighters_map[block]
+                def hliter(code):
+                    code = deindent_snippet(code)
+                    hblock = highlight(code, lexer(), HtmlFormatter(noclasses=True))
+                    self.write_raw(hblock.strip())
+                return hliter
+
+        if name in ('on_text_h2', 'on_text_h3', 'on_text_h4'):
+            def header(text):
+                hlevel = int(name[-1])
+                self.write_raw('<br><h{0}>'.format(hlevel))
+                self.write_text(text)
+                self.write_raw('</h{0}>'.format(hlevel))
+            return header
+
+        raise AttributeError("type %r has no attribute %s" % (self.__class__, name))
+
+    def on_text_h1(self, text):
+        # skip main header for blogspot
+        pass
+
     def on_img(self, url):
         url = url.strip()
         if url.endswith('svg'):
-            self.write('<object data="{0}" type="image/svg+xml"></object>'.format(url)) 
+            self.write_raw('<object data="{0}" type="image/svg+xml"></object>'.format(url)) 
         else:
-            self.write('<br><img src="{0}" width="740" /><br>'.format(url))
+            self.write_raw('<br><img src="{0}" width="740" /><br>'.format(url))
 
+    def do_href(self, ref_descr):        
+        self.write_raw(
+            self.process_href(
+                re_href.match(ref_descr))) 
+    
+    def on_list(self, items):
+        self.write_raw("<ul>")
+        for item in items:
+            self.write_raw("<li>")
+            self.on_text(item, no_para=True)
+        self.write_raw("</ul>")
+    
     def on_linklist(self, block):
-        self.write(u"Ccылки:<br>")
         for line in block.split('\n'):
             line = line.strip()
-            self.do_href(line)
-            self.write("<br>")
+            
+            if line == "":
+                continue
 
-    def do_href(self, ref_descr):
-        self.write(self.process_href(re_href.match(ref_descr))) 
+            if 'http://' in line:
+                name, url = line.split('http://', 1)
+                name = name.strip()
+                url = "http://" + url
+            elif 'https://' in line:
+                name, url = line.split('https://', 1)
+                name = name.strip()
+                url = "https://" + url
+            else:
+                raise ValueError("Can't process linklist item {0!r}".format(line))
+            
+            self.write_raw('<p style="text-indent:20px"><a name="{0}">'.format(
+                    escape_html(name.replace(' ', '_'))))
+
+            self.do_href(url)
+
+            self.write_raw('</a></p>')
+
+    def process_backref(self, ref_descr):
+        gr1 = ref_descr.group(1)
+        return '<a href="#{0}">{1}</a>'.format(
+                    escape_html(gr1.replace(' ', '_'), 
+                    escape_html(gr1)))
                
-    def on_list(self, items):
-        self.write("<ul>")
-        for item in items:
-            self.write("<li>")
-            self.on_text(item, no_para=True)
-        self.write("</ul>")
-
-    def on_c(self, block):
-        self.write(highlight_c(deindent_snippet(block)).strip())
-
-    def on_header1(self, block):
-        self.write(u"<br><h3>{0}</h3>".format(self.escape_html(block)) + '\n')
-
-    def on_header2(self, block):
-        self.write(u"<br><h4>{0}</h4>".format(self.escape_html(block)) + '\n')
-    
     def process_href(self, mobj):
+        name = mobj.group('name')
         g1 = mobj.group('proto')
         g2 = mobj.group('url')
-        name = mobj.group('name')
 
         if g2[-1] in '.,':
             add_symbol = g2[-1]
             g2 = g2[:-1]
         else:
             add_symbol = ""
-
-        if not name:
+        
+        if name is None:
             name = g2
         else:
             name = name[1:-1]
-
-        #print repr(g1), repr(g2), repr(name), repr(add_symbol)
 
         url = g1 + g2
         self.refs.append( url )
         
         return u'<a href="{0}">{1}</a>{2}'.format(url, name, add_symbol)
 
-    def escape_html(self, text):
-        ntext = escape_html(text)
-        ntext = re_bold.sub(ur"<b>\1</b>", ntext)
-        return re_href.sub(self.process_href, ntext)
-    
     def write_footer(self):
-        self.write(u'Исходники этого и других постов со скриптами лежат тут - ')
+        self.write_raw(u'Исходники этого и других постов со скриптами лежат тут - ')
         self.do_href("[github.com/koder-ua]https://github.com/koder-ua/python-lectures.")
-        self.write(u'При использовании их, пожалуйста, ссылайтесь на ')
+        self.write_raw(u'При использовании их, пожалуйста, ссылайтесь на ')
         self.do_href("[koder-ua.blogspot.com]http://koder-ua.blogspot.com/.")
  
 
-def not_so_rest_to_html(text, styles):
-    formatter = BlogspotHTMLProvider()
+def debug_block(block_type, data):
+    print block_type 
+    print 
+    if isinstance(data, basestring):
+        print data.encode('utf8')
+    elif isinstance(data, (list, tuple)):
+        print (u"\n >>>> " + u"\n >>>> ".join(data)).encode('utf8')
+    else:
+        print repr(data)
+
+    print "~~" * 50
+    print
+
+def not_so_rest_to_xxx(text, styles, formatter):
 
     text = text.replace('\t', ' ' * 4)
+    
     # skip header
     text = text.split('\n', 3)[3]
 
-    for block_data in find_blocks(text):
-        for block_type, btext in classify_block(block_data):
+    for block_type, data in parse(text):
+        
+        if block_type in styles:
+             block_type, style = styles[block_type]
+        else:
+             style = None
 
-            if block_type in styles:
-                block_type, style = styles[block_type]
-            else:
-                style = None
-
-            formatter.process(block_type, btext, style)
+        formatter.process(block_type, data, style)
     
     formatter.write_footer()
     return formatter.get_result()
@@ -340,15 +278,17 @@ def main(argv=None):
 
     parser.add_option("-s", "--style-files", dest='style_files', default='notsores_styles.txt')
     parser.add_option("-o", "--output-file", dest='output_file', default=None)
-    parser.add_option("-f", "--format", dest='format', default='html')
+    parser.add_option("-f", "--format", dest='format', default='blogspot')
     
     opts, files = parser.parse_args(argv)
 
     if len(files) < 2:
         print "Error - no template files"
+        return 1
 
     if len(files) > 2:
         print "Error - only one template file per call allowed"
+        return 1
     
     fname = files[1]
     fc = open(fname).read().decode('utf8')
@@ -361,24 +301,27 @@ def main(argv=None):
         for style_fname in opts.style_files.split(':'):
             styles.update(parse_style_file(style_fname))
 
-    import pprint
+    formatters = {
+        'blogspot' : BlogspotHTMLProvider()
+    }
 
-    pprint.pprint(styles)
-
-    if opts.format == 'html':
-        res = not_so_rest_to_html(fc, styles)
+    if opts.format not in formatters:
+        print >>sys.stderr, "Unknown format {0!r} only '{1}'' formats are supported"\
+                    .format(opts.format, ",".join(formatters.keys()))
+        return 1
     else:
-        print "Unknown format " + repr(opts.format)
+        res = not_so_rest_to_xxx(fc, styles, formatters[opts.format])
     
-    if opts.output_file is None:
-        res_fname = os.path.splitext(fname)[0] + '.html'
-    else:
-        res_fname = opts.output_file
+        if opts.output_file is None:
+            res_fname = os.path.splitext(fname)[0] + '.html'
+        else:
+            res_fname = opts.output_file
 
-    open(res_fname, "w").write(res.encode("utf8"))
+        open(res_fname, "w").write(res.encode("utf8"))
+        return 0
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main(sys.argv))
 
 
 
