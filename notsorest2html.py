a628b866e57b557bb1c253a28d6483c753dcda47
kdanilov <kdanilov@mirantis.com>
2013-11-14 02:33:17 +0200

    add code affinity post

--- notsorest2html.py
+++ notsorest2html.py
@@ -41,7 +41,7 @@ def escape_html(text, esc_all=False):
 
     if esc_all:
         html_escape_table["'"] = '&#39;'
-    
+
     return "".join( html_escape_table.get(c, c) for c in text)
 
 
@@ -64,28 +64,30 @@ assert re_striked2.match('-b-')
 assert re_striked1.search(' -b-')
 assert re_striked1.search(' -b- ')
 
-re_backref = re.compile(ur"""(?u)\[\s*([- \w.|()'"]+?)\s*\]""")
-re_href = re.compile(r"(?u)(?P<name>\[\s*([- _\w/.()|]+)\s*\])?(?P<proto>https?://)(?P<url>.*?)(?=\s|$)")
+re_backref = re.compile(ur"""(?u)\[\s*([-, \w.|()'"]+?)\s*\]""")
+re_href = re.compile(r"(?u)(?P<name>\[\s*([- _\w/.()|]+)\s*\])?" +
+                     r"(?P<proto>https?://)(?P<url>.*?)(?=\s|$)")
+
 
 class NotSoRESTHandler(object):
     def __init__(self, opts):
         self.stream = []
         self.opts = opts
-    
+
     def write_raw(self, text):
         self.stream.append(text)
-    
+
     def get_result(self):
         return "".join(self.stream)
-    
+
     def set_result(self, res):
         self.stream = res
-    
+
     def process(self, block):
 
         if block.style is not None:
             self.start_style(block.style)
-        
+
         self.block_opts = block.opts
 
         tp = block.tp.split('.')
@@ -131,13 +133,13 @@ hide_show_func = """
         function on_double_hidabble_click()
         {
             var me = $(this);
-            
+
             var hide_id1 = me.attr("objtohide1");
             var hide_id2 = me.attr("objtohide2");
-            
+
             var controlled_object1 = $('#' + hide_id1);
             var controlled_object2 = $('#' + hide_id2);
-            
+
             controlled_object1.toggle();
             controlled_object2.toggle();
 
@@ -153,6 +155,7 @@ hide_show_func = """
 """
 show_hide_block = ""
 
+
 class Reporter(object):
     def __init__(self):
         self.messages = []
@@ -160,23 +163,28 @@ class Reporter(object):
     def add_message(self, tp, params, message):
         #print "I get message", tp, message
         self.messages.append((tp, params, message))
-    
+
     def display_results(self, *dt, **mp):
         pass
+
 
 class Stdout_replacer(object):
     def write(self, data):
         pass
 
+
 def check_python_code(code, line, use_lint=True, imp_mod=False):
 
-    code = "# -*- coding:utf8 -*-\nfrom oktest import ok\n" + code.encode("utf8")
+    code = "# -*- coding:utf8 -*-\nfrom oktest import ok\n" + \
+           code.encode("utf8")
 
     try:
         compile(code, "<opt_file>", 'exec')
     except SyntaxError as err:
         err.lineno += line - 2
-        err.args = (err.args[0], (err.args[1][0], err.args[1][1] + line - 2, err.args[1][2]))
+        err.args = (err.args[0],
+                    (err.args[1][0],
+                    err.args[1][1] + line - 2, err.args[1][2]))
         raise err
 
     with warnings.catch_warnings():
@@ -185,7 +193,7 @@ def check_python_code(code, line, use_lint=True, imp_mod=False):
 
     os.mkdir(dname)
     fname = os.path.join(dname, "module.py")
-    
+
     open(fname, 'w').write(code)
 
     try:
@@ -194,10 +202,11 @@ def check_python_code(code, line, use_lint=True, imp_mod=False):
 
             try:
                 stderr = sys.stderr
-                sys.stderr = Stdout_replacer()  
+                sys.stderr = Stdout_replacer()
                 logilab.astng.builder.MANAGER.astng_cache.clear()
-                rcfile = os.path.join(os.path.abspath(os.path.dirname(__file__)), "pylintrc")
-                lint.Run( [fname, '--rcfile=' + rcfile], rep, exit=False)
+                rcfile = os.path.join(os.path.abspath(
+                                      os.path.dirname(__file__)), "pylintrc")
+                lint.Run([fname, '--rcfile=' + rcfile], rep, exit=False)
                 #lint.PyLinter( [fname], rep)
             finally:
                 sys.stderr = stderr
@@ -209,7 +218,7 @@ def check_python_code(code, line, use_lint=True, imp_mod=False):
                     print "Python block in line {0}: {1} {2}".format(
                             line + data[3] - 2, # we add two lines to the top og the file
                             tp, msg)
-        
+
         if imp_mod:
             try:
                 sys.path.insert(0, dname)
@@ -220,7 +229,7 @@ def check_python_code(code, line, use_lint=True, imp_mod=False):
                 del sys.path[0]
                 del sys.modules['module']
 
-    finally:    
+    finally:
         os.unlink(fname)
         os.rmdir(dname)
 
@@ -239,7 +248,7 @@ hide_show2 = u'<a hided_text="{hided_text}" ' + \
              u'class="dhidder" objtohide1="{hided_id1}" objtohide2="{hided_id2}" >{default_text}</a>'
 
 class BlogspotHTMLProvider(NotSoRESTHandler):
-    
+
     HREF_PREFIX = "_a_href_"
 
     def __init__(self, opts):
@@ -252,10 +261,10 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
 
         if self.opts.standalone:
             self.write_raw("<html><head>")
-            self.write_raw('<meta http-equiv="Content-Type" content="text/html; charset=utf-8">')            
+            self.write_raw('<meta http-equiv="Content-Type" content="text/html; charset=utf-8">')
             self.write_raw("</head><body>")
-                
-        
+
+
         self.write_raw('<script type="text/javascript" ' + \
                        'src="http://ajax.googleapis.com/ajax/' + \
                        'libs/jquery/1.7.1/jquery.min.js"></script>\n')
@@ -264,9 +273,9 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
         self.write_raw(self.text_to_html(text).replace('\n', ' '))
 
     def text_to_html(self, text):
-        
+
         ntext = escape_html(text)
-        
+
         ntext = re_bold.sub(r"<b>\1</b>", ntext)
         ntext = re_it.sub(r"<i>\1</i>", ntext)
         ntext = re_striked.sub(r"<s>\1</s>", ntext)
@@ -276,7 +285,7 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
             ntext = re_bold1.sub(r"<b>\1</b>", ntext)
             ntext = re_it1.sub(r"<i>\1</i>", ntext)
             ntext = re_striked1.sub(r"<s>\1</s>", ntext)
-            
+
             ntext = re_bold2.sub(r"<b>\1</b>", ntext)
             ntext = re_it2.sub(r"<i>\1</i>", ntext)
             ntext = re_striked2.sub(r"<s>\1</s>", ntext)
@@ -285,11 +294,11 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
         ntext = re_href.sub(self.process_href, ntext)
 
         return ntext
-    
+
     def on_open_hide(self):
         oid = str(uuid.uuid1()).replace("-", "")
         self.write_raw((hide_show + "<br>" + hide_show_span).format(
-                                        hided_text=u"Показать код", 
+                                        hided_text=u"Показать код",
                                         visible_text=u"Скрыть код",
                                         hided_id=oid,
                                         default_style='style="display:none"',
@@ -300,7 +309,7 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
 
     def start_style(self, style):
         self.write_raw('<span style="{0}">'.format(style))
-    
+
     def end_style(self, style):
         self.write_raw('</span>')
 
@@ -314,19 +323,19 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
         if block != "":
             if not no_para:
                 self.write_raw('<p style="text-indent:20px">')
-            
+
             self.write_text(block)
-            
+
             if not no_para:
                 self.write_raw("</p>")
-    
+
     def on_cut(self, block):
         self.write_raw("<!--more-->")
         self.found_splitter = True
 
     def on_raw(self, block):
-        self.write_raw('<pre><font face="courier">' + 
-                       escape_html(block, esc_all=True) + 
+        self.write_raw('<pre><font face="courier">' +
+                       escape_html(block, esc_all=True) +
                         '</font></pre>')
 
     highlighters_map = {}
@@ -347,7 +356,7 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
                 lexer = self.highlighters_map[block]
                 def hliter(code, line):
                     code = deindent_snippet(code)
-                    
+
                     if block == 'python':
                         opts = self.block_opts if self.block_opts is not None else tuple()
 
@@ -358,7 +367,7 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
                             use_lint = False
 
                         check_python_code(code, line, use_lint=use_lint, imp_mod=imp_mod)
-                    
+
                     splits = re.split(r"\n#----*\n", code)
 
                     if len(splits) == 1:
@@ -373,7 +382,7 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
                     oid_raw = str(uuid.uuid1()).replace("-", "")
 
                     self.write_raw(
-                        hide_show2.format(hided_text=u"С подсветкой синтаксиса", 
+                        hide_show2.format(hided_text=u"С подсветкой синтаксиса",
                                           visible_text=u"Без подсветки синтаксиса",
                                           hided_id1=oid_code,
                                           hided_id2=oid_raw,
@@ -384,7 +393,7 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
                     self.write_raw(hblock.strip())
                     self.write_raw("</span>")
 
-                    self.write_raw(hide_show_span.format(hided_id=oid_raw, 
+                    self.write_raw(hide_show_span.format(hided_id=oid_raw,
                                         default_style='style="line-height:100%;display:none"'))
                     self.on_raw(raw)
                     self.write_raw("</span>")
@@ -398,7 +407,7 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
                 self.write_text(text)
                 self.write_raw('</h{0}>'.format(hlevel))
             return header
-        
+
         if name == 'on_text_h1':
             return lambda x : None
 
@@ -412,30 +421,30 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
         url = url.strip()
         iwith = self.block_opts.get('with','700')
         if url.endswith('svg'):
-            self.write_raw('<object data="{0}" type="image/svg+xml"></object>'.format(url)) 
+            self.write_raw('<object data="{0}" type="image/svg+xml"></object>'.format(url))
         else:
             self.write_raw('<br><img src="{0}" width="{1}" /><br>'.format(url, iwith))
 
-    def do_href(self, ref_descr):        
+    def do_href(self, ref_descr):
         self.write_raw(
             self.process_href(
-                re_href.match(ref_descr))) 
-    
+                re_href.match(ref_descr)))
+
     def on_list(self, items):
-        
+
         self.write_raw("<ul>")
         for item in items:
             self.write_raw("<li>")
             self.on_text(item, no_para=True)
         self.write_raw("</ul>")
-    
+
     def on_linklist(self, block):
         self.on_text(u"Ссылки:", no_para=True)
         self.write_raw("<br>")
 
         for line in block.split('\n'):
             line = line.strip()
-            
+
             if line == "":
                 continue
 
@@ -449,7 +458,7 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
                 url = "https://" + url
             else:
                 raise ValueError("Can't process linklist item {0!r}".format(line))
-            
+
             self.write_raw('&nbsp;' * 10)
             if name:
                 name = name.replace(' ', '_')
@@ -460,7 +469,7 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
 
             if name:
                 self.write_raw('</a>')
-            
+
             self.write_raw('<br>')
 
     def process_backref(self, ref_descr):
@@ -472,11 +481,11 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
             text = name = gr1
 
         name = name.replace(' ', '_')
-        
+
         self.backref_list.append(name)
 
         return u'<a href="{0}">{1}</a>'.format(self.HREF_PREFIX + name, text)
-               
+
     def process_href(self, mobj):
         name = mobj.group('name')
         g1 = mobj.group('proto')
@@ -487,7 +496,7 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
             g2 = g2[:-1]
         else:
             add_symbol = ""
-        
+
         if name is None:
             name = g2
             if name.endswith('/'):
@@ -497,7 +506,7 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
 
         url = g1 + g2
         self.refs.append( url )
-        
+
         return u'<a href="{0}">{1}</a>{2}'.format(url, name, add_symbol)
 
     def finalize(self):
@@ -529,13 +538,13 @@ class BlogspotHTMLProvider(NotSoRESTHandler):
 
         for name, val in self.href_map.items():
             res = res.replace(self.HREF_PREFIX + name, val)
-        
+
         self.set_result([res])
- 
+
 
 def debug_block(block):
-    print block.type 
-    print 
+    print block.type
+    print
     if isinstance(block.data, basestring):
         print block.data.encode('utf8')
     elif isinstance(block.data, (list, tuple)):
@@ -549,9 +558,9 @@ def debug_block(block):
 def not_so_rest_to_xxx(text, styles, formatter):
 
     text = text.replace('\t', ' ' * 4)
-    
+
     for block in parse(text):
-        
+
         #debug_block(block)
 
         if block.tp in styles:
@@ -560,7 +569,7 @@ def not_so_rest_to_xxx(text, styles, formatter):
              style = None
 
         formatter.process(block)
-    
+
     formatter.finalize()
     return formatter.get_result()
 
@@ -573,11 +582,11 @@ def parse_style_file(fname):
 
         if line.startswith('#'):
             continue
-        
+
         mres = style_cmd_re.match(line)
         if not mres:
             raise RuntimeError("Error in style file {0!r} in line {1}".format(fname, lnum))
-        
+
         res[mres.group('new_style')] = (mres.group('old_style'), mres.group('opts'))
     return res
 
@@ -586,7 +595,7 @@ def main(argv=None):
     import optparse
 
     argv = argv or sys.argv
-    
+
     parser = optparse.OptionParser()
 
     parser.add_option("-s", "--style-files", dest='style_files', default='__def__')
@@ -596,7 +605,7 @@ def main(argv=None):
     parser.add_option("-l", "--old-inline-support", dest='old_inline_support', default=False, action='store_true')
     parser.add_option("-a", "--standalone", dest='standalone', default=False,
                         action='store_true')
-    
+
     opts, files = parser.parse_args(argv)
 
     if len(files) < 2:
@@ -606,10 +615,10 @@ def main(argv=None):
     if len(files) > 2:
         print "Error - only one template file per call allowed"
         return 1
-    
+
     fname = files[1]
     fc = open(fname).read().decode('utf8')
-    
+
     styles = {}
 
     # {new_style : (old_style, css)}
@@ -617,7 +626,7 @@ def main(argv=None):
     if opts.style_files != '':
         for style_fname in opts.style_files.split(':'):
             if style_fname == '__def__':
-                style_fname = os.path.join(os.path.split(__file__)[0], 
+                style_fname = os.path.join(os.path.split(__file__)[0],
                                            'notsores_styles.txt')
             styles.update(parse_style_file(style_fname))
 
@@ -631,12 +640,12 @@ def main(argv=None):
         return 1
     else:
         res = not_so_rest_to_xxx(fc, styles, formatters[opts.format])
-    
+
         if opts.output_file is None:
             res_fname = os.path.splitext(fname)[0] + '.html'
         else:
             res_fname = opts.output_file
-        
+
         open(res_fname, "w").write(res.encode("utf8"))
         return 0
 
